f5bcc229abe5ad23f4927ec8e47d9bd4f3991578
priyanka <priyankaselvaraj0306@gmail.com>
2025-08-11 15:36:11 +0530

    changes in cat

--- AugustGT/src/sample/Cat.java
+++ AugustGT/src/sample/Cat.java
@@ -4,6 +4,8 @@ public class Cat {
 
 	public void animal() {
 		System.out.println("Cat says meow");
+		System.out.println("Cat says meow");
+
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
